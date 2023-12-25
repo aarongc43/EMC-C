325fa5ea10645c318a3b37603d29f42918f2f16d
aarongc43 <aarongc43@gmail.com>
2023-12-24 23:43:48 -0600

    modified:   main 	modified:   main.c

--- main.c
+++ main.c
@@ -13,6 +13,11 @@ unsigned char* code_chunk(size_t* size) {
     return 0;
 }
 
+unsigned char* atom_chunk(size_t* size) {
+    *size = 0;
+    return 0;
+}
+
 void addUInt32ToByteContainerBigEndian(unsigned char **byteContainer, size_t *currentSize, size_t *capacity, uint32_t value) {
     for (int i = 3; i >= 0; i--) {
         unsigned char byte = (value >> (i * 8)) & 0xFF;
@@ -71,7 +76,7 @@ int write_to_file(const char *filename, const unsigned char *bytes, size_t size)
 
     if (fclose(file) != 0) {
         perror("Error closing file");
-        return-1;
+        return -1;
     }
 
     return 0;
@@ -85,35 +90,74 @@ int main(int argc, char *argv[]) {
     }
     */
 
-    // byte container creation 
-    size_t capacity = 20;
-    size_t currentSize = 0;
-    unsigned char* byteContainer = malloc(capacity * sizeof(unsigned char));
-    if (byteContainer == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
+    const char* chunkName = "Code";
+    const uint32_t instructionSet = 0;
+    const uint32_t opcodeMax = 169;
+    const uint32_t labelCount = 7;
+    const uint32_t functionCount = 3;
+
+    size_t codeDataSize = 0;
+    unsigned char* codeData = NULL;
+
+    uint32_t subSize = 4 * sizeof(uint32_t);
+    uint32_t chunkSize = subSize + codeDataSize;
+
+    // BEAM container
+    size_t beamCapacity = 20;
+    size_t beamSize = 0;
+    unsigned char* beam = malloc(beamCapacity * sizeof(unsigned char));
+    if (beam == NULL) {
+        fprintf(stderr, "Memory allocation failed for beam.\n");
         return 1;
     }
 
-    addStringToByteContainer(&byteContainer, &currentSize, &capacity, "FOR1");
+    addStringToByteContainer(&beam, &beamSize, &beamCapacity, "BEAM");
 
     // calculate code chunk size
     size_t codeSize;
     unsigned char* code = code_chunk(&codeSize);
-
-    uint32_t totalSize = 4 + codeSize;
-    addUInt32ToByteContainerBigEndian(&byteContainer, &currentSize, &capacity, totalSize);
-
-    addStringToByteContainer(&byteContainer, &currentSize, &capacity, "BEAM");
-
-    for (size_t i = 0; i <codeSize; i++) {
-        addByte(&byteContainer, &currentSize, &capacity, code[i]);
+    for (size_t i = 0; i < codeSize; i++) {
+        addByte(&beam, &beamSize, &beamCapacity, code[i]);
     }
 
-    if (write_to_file("main.beam", byteContainer, currentSize) != 0) {
-        free(byteContainer);
+    // byte container creation 
+    size_t bytesCapacity = 40;
+    size_t bytesSize = 0;
+    unsigned char* bytes = malloc(bytesCapacity * sizeof(unsigned char));
+    if (bytes == NULL) {
+        fprintf(stderr, "Memory allocation failed for bytes.\n");
+        return 1;
+    }
+
+    addStringToByteContainer(&bytes, &bytesSize, &bytesCapacity, "FOR1");
+    uint32_t beamLength = beamSize;
+    addUInt32ToByteContainerBigEndian(&bytes, &bytesSize, &bytesCapacity, beamLength);
+
+    for (size_t i = 0; i < beamSize; i++) {
+        addByte(&bytes, &bytesSize, &bytesCapacity, beam[i]);
+    }
+
+    addStringToByteContainer(&beam, &beamSize, &beamCapacity, chunkName);
+    chunkSize = subSize + codeDataSize;
+    // calculate chunksize based of content that will be added
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, chunkSize);
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, subSize);
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, instructionSet);
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, opcodeMax);
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, labelCount);
+    addUInt32ToByteContainerBigEndian(&beam, &beamSize, &beamCapacity, functionCount);
+
+    for (size_t i = 0; i < codeDataSize; i++) {
+        addByte(&beam, &beamSize, &beamCapacity, codeData[i]);
+    }
+
+    if (write_to_file("main.beam", bytes, bytesSize) != 0) {
+        free(bytes);
+        free(beam);
         return -1;
     }
 
-    free(byteContainer);
+    free(bytes);
+    free(beam);
     return 0;
 }
